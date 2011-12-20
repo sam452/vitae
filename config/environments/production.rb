a29db282dfbcbd455238ef5f2ab24d9ad0fff7be
sam walton <sam@foofrancine.com>
2011-12-20 12:40:56 -0600

    open up all precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,10 +43,10 @@ Vitae::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
-  # config.assets.precompile += %w( blueprint/screen.css blueprint/print.css )
-  #config.assets.precompile += %w( *.css *.js )
-  config.assets.compile = false
+  config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( blueprint/screen.css blueprint/print.css )
+  config.assets.precompile += %w( *.css *.js )
+  #config.assets.compile = false
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
