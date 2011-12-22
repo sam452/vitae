c8a178494f90f5e0bf04720fe15d19b6b2cf15a6
sam walton <sam@foofrancine.com>
2011-12-22 11:00:33 -0600

    finished hartl chap 7

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111221161736) do
+ActiveRecord::Schema.define(:version => 20111222135124) do
 
   create_table "chairs", :force => true do |t|
     t.string   "name"
@@ -58,6 +58,8 @@ ActiveRecord::Schema.define(:version => 20111221161736) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
