1a7b9784e6712ace2c87ff299deb9834b7ed80d1
sam walton <sam@foofrancine.com>
2011-12-23 11:22:16 -0600

    finished hartl chap 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,19 @@ class UsersController < ApplicationController
   end
   
   def new
+    @user = User.new
+    @title = "Sign up"
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save # handle a successful save
+      flash[:success] = "Welcome to Vitae"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
   end
 
 end
