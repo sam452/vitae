91acdc09c091984bee5615fc83e57ede383bf301
sam walton <sam@foofrancine.com>
2012-01-01 12:30:51 -0600

    reset back to 10 before filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,11 +12,28 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save # handle a successful save
+      sign_in @user
       flash[:success] = "Welcome to Vitae"
       redirect_to @user
     else
       @title = "Sign up"
       render 'new'
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit user"
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
     end
   end
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,6 +9,10 @@ module SessionsHelper
     @current_user = user
   end
   
+  def current_user
+    @current_user ||= user_from_remember_token
+  end
+  
   def signed_in?
     !current_user.nil?
   end
